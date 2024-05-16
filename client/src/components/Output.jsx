fc58678eedc5e64726b579d097a3081939153d6f
Astha Negi <asthanegi539@gmail.com>
2024-05-16 21:21:41 +0530

    updated

--- client/src/components/Output.jsx
+++ client/src/components/Output.jsx
@@ -14,8 +14,8 @@ const Output = ({ editorRef, language }) => {
         if (!sourceCode) return;
         try {
             setIsLoading(true);
-            console.log("port = " + import.meta.env.VITE_BACKEND_URL + "/compile");
-            const response = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/compile`, { sourceCode, userInput });
+            console.log("port = " + import.meta.env.REACT_APP_API_URL + "/compile");
+            const response = await axios.post(`${import.meta.env.REACT_APP_API_URL}/compile`, { sourceCode, userInput });
 
             if (response.data.stderr) {
                 setOutput(response.data.stderr);
